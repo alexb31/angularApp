6b30f107ff66f97623dcf02f8b330a61d91e210b
alexb31 <alexandrebornstein@gmail.com>
2018-05-04 22:31:56 +0200

    ngModel & Data Binding

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,10 +8,20 @@ import { User } from '../../models/User';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  user: User = {
+    firstName: '',
+    lastName: '',
+    age: null,
+    adress: {
+      street: '',
+      city: '',
+      state: ''
+    }
+  }
   users: User[];
   showExtended: boolean = true;
   loaded: boolean = false;
-  enableAdd: boolean = true;
+  enableAdd: boolean = false;
   showUserForm: boolean  = false;
 
   constructor() { }
@@ -62,8 +72,22 @@ export class UsersComponent implements OnInit {
       this.loaded = true;
   }
 
-  addUser(user: User) {
-    this.users.push(user);
+  addUser() {
+    this.user.isActive = true;
+    this.user.registered = new Date();
+
+    this.users.unshift(this.user);
+
+    this.user = {
+      firstName: '',
+      lastName: '',
+      age: null,
+      adress: {
+      street: '',
+      city: '',
+      state: ''
+    }
+    }
   }
 
   // toggleHide(user) {
